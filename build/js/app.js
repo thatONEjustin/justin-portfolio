fa724be98f5e997dfad001c867c55c5381cc8961
thatONEjustin <justinpaelmo@gmail.com>
2016-09-20 20:35:04 -0400

    comments

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,5 @@
+//Basic routing, I made this a 1 page site so I load everything through 
+//pages/home.html
 var app = angular.module('portfolio-site', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
@@ -9,12 +11,15 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
         .otherwise({ redirectTo: '/' });
 }]);
 
+//Basically my use of MainController mirrors an old technique I used to do 
+//with an init function that ran when $(document).ready() would fire. 
 app.controller('MainController', function ($scope, $http, GetContent) {
 
     try {
 
         Typekit.load({ async: true });
 
+        //I chose to load the .json data here making it part of the main load process. 
         GetContent.getJson('./data/content.json').then(function (data) {
             $scope.content = data;
         });
@@ -33,6 +38,7 @@ app.controller('MainController', function ($scope, $http, GetContent) {
 
 app.controller('HomeController', function ($scope) {
 
+    //Here I set the $scope variables from the $scope.content data derived from content.json
     $scope.title = $scope.content.title;
     $scope.tagline = $scope.content.tagline;
     $scope.disclaimer = $scope.content.disclaimer;
@@ -44,6 +50,8 @@ app.controller('HomeController', function ($scope) {
 
 });
 
+//Created a custom service to be able to load .JSON files into variables. the way this promise is set up 
+//is so that files aren't loaded and code is run before the data is there.
 app.service('GetContent', function ($http) {
     return {
         getJson : function(url) {
