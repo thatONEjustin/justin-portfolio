cc29a4fbd9d795baeadc57ece1456a80262af252
thatONEjustin <justinpaelmo@gmail.com>
2016-09-21 13:42:24 -0400

    mobile changes

--- build/js/app.js
+++ build/js/app.js
@@ -13,20 +13,12 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
 //Basically my use of MainController mirrors an old technique I used to do 
 //with an init function that ran when $(document).ready() would fire. 
-app.controller('MainController', function ($scope, $http, GetContent) {
+app.controller('MainController', function ($scope) {
 
     try {
 
         Typekit.load({ async: true });
 
-        //I chose to load the .json data here making it part of the main load process. 
-        GetContent.getJson('./data/content.json').then(function (data) {
-            $scope.content = data;
-        });
-    
-        GetContent.getJson('./data/projects.json').then(function (data) {
-            $scope.projects = data; 
-        });
 
     } catch (err) {
 
@@ -36,18 +28,29 @@ app.controller('MainController', function ($scope, $http, GetContent) {
 
 });
 
-app.controller('HomeController', function ($scope) {
+app.controller('HomeController', function ($scope, GetContent) {
 
-    //Here I set the $scope variables from the $scope.content data derived from content.json
-    $scope.title = $scope.content.title;
-    $scope.tagline = $scope.content.tagline;
-    $scope.disclaimer = $scope.content.disclaimer;
-    $scope.mission = $scope.content.mission;
-    $scope.statement = $scope.content.statement;
-    $scope.languageTitle = $scope.content.languageTitle;
-    $scope.languages = $scope.content.languages;
-    $scope.projectTitle = $scope.content.projectTitle;
+    //I chose to load the .json data here making it part of the main load process. 
+    GetContent.getJson('./data/content.json').then(function (data) {
+        $scope.content = data;
 
+            
+        //Here I set the $scope variables from the $scope.content data derived from content.json
+        $scope.title = $scope.content.title;
+        $scope.tagline = $scope.content.tagline;
+        $scope.disclaimer = $scope.content.disclaimer;
+        $scope.mission = $scope.content.mission;
+        $scope.statement = $scope.content.statement;
+        $scope.languageTitle = $scope.content.languageTitle;
+        $scope.languages = $scope.content.languages;
+        $scope.projectTitle = $scope.content.projectTitle;
+
+    });
+
+    GetContent.getJson('./data/projects.json').then(function (data) {
+        $scope.projects = data; 
+    });
+    
 });
 
 //Created a custom service to be able to load .JSON files into variables. the way this promise is set up 
